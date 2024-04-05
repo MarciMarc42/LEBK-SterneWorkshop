89e2c2e1db8a4d4d9da0fdf14383370854e8b19f
braeuer <braeuer@adesso-mobile.de>
2024-04-05 15:12:35 +0200

    adds basic project setup

---
+++ src/test/java/de/adessomobile/lebk/workshop/WorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package de.adessomobile.lebk.workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
